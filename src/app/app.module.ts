33d82a86a10f78dc0cc81f68a96808da49208398
atck3r <androidmini92@gmail.com>
2024-08-31 20:26:14 +0300

    Add ProductList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
