d2ccb246e32e3a1ff1d8cce5774b4f39f1c25915
atck3r <androidmini92@gmail.com>
2024-08-31 23:39:47 +0300

    Improve Service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -8,8 +8,27 @@ export class CartService {
   products: Cart[] = [];
   constructor() {}
 
-  addToCart(p: Cart) {
-    this.products.unshift(p);
+  addToCart(p: Cart): void {
+    const idx = this.products.findIndex(e => e.id === p.id);
+    if (idx !== -1) {
+      this.products[idx].quantity += p.quantity;
+    } else {
+      this.products.unshift(p);
+    }
   }
 
+  updateProduct(p: Cart): Cart[] {
+    this.products = this.products.map(c => {
+      if (p.id === c.id) {
+        return p;
+      }
+      return c;
+    })
+    return this.products;
+  }
+
+  removeProducts(p: Cart): Cart[] {
+    this.products = this.products.filter(c => c.id !== p.id);
+    return this.products
+  }
 }
