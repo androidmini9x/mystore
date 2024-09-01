105c9060370363d2683b24f35e4a5dc1a7da629d
atck3r <androidmini92@gmail.com>
2024-09-01 19:15:00 +0300

    Add Order Path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetialComponent } from './components/product-detial/product-detial.component';
+import { OrderConfirmationComponent } from './components/order-confirmation/order-confirmation.component';
 
 const routes: Routes = [
   {path: '', component: ProductsListComponent},
   {path: 'cart', component: CartComponent},
-  {path: 'products/:id', component: ProductDetialComponent}
+  {path: 'products/:id', component: ProductDetialComponent},
+  {path: 'order-confirmation', component: OrderConfirmationComponent}
 ];
 
 @NgModule({
